464ff67bbf26c840e5dace1ab4155dd4e56fac92
gvyganovsky <gvyganovsky@gmail.com>
2024-02-01 18:42:08 +0300

    login

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.contrib.auth.views import LoginView, LogoutView
 
@@ -13,8 +13,10 @@ urlpatterns = [
 
     path('product/<int:product_id>', views.product),
 
-    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    # path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    # path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+
+    path('accounts/', include('django.contrib.auth.urls')),
 
     path('', views.index)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
